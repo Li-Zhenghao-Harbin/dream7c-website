abc08ba60d1f1bfe03dcb6f42957c5b14d5771be
Li-Zhenghao-Harbin <dream7c@126.com>
2026-05-20 19:04:15 +0800

    产品下载页面数据填充优化

---
+++ js/data/product-download-pages.js
@@ -0,0 +1,375 @@
+var productDownloadPageData = {
+    DE: {
+        title: "下载 柒幻 寒露",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 寒露",
+                version: "v 1.1.5.1",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "1 MB",
+                updateDate: "2022.01.25",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载（Windows版）",
+                downloadHref: "http://www.dream7c.com/柒幻 寒露 v 1.1.5.1c.rar",
+                thanksText: "感谢您使用 柒幻 寒露",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.1.5.1&nbsp;(2022.01.25)</p>*支持搜索题目<br/>*添加更多快捷键<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.1.4.8&nbsp;(2021.12.19)</p>*界面优化<br/>*支持从剪切板快速导入题目（Ctrl+Q）<br/>*支持通过鼠标拖拽导入项目文件<br/>*修改导出和解析XML文件的格式<br/>*修改默认配置<br/>*修复保存文件时可能出现的异常<br/>*修复多窗口模式下切换窗口时可能出现的定位异常<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.1.1&nbsp;(2021.10.16)</p>*界面优化<br/>*修复不同分辨率的设备下放映时可能出现的异常<br/>*修复导出文本文档时的格式错误<br/>*修复打开文件时可能出现的异常<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.0.5&nbsp;beta&nbsp;(2021.09.30)</p>*界面优化<br/>*修复添加和重新选择问题时可能出现的显示错误<hr/>' +
+                    '历史版本 alpha (2021.09.27)'
+            },
+            {
+                tabLabel: "Android版",
+                downloadTitle: "柒幻 寒露 Android",
+                version: "v 1.1.6",
+                supportLabel: "支持系统",
+                supportValue: "Android",
+                language: "中文(简体)",
+                size: "2.25 MB",
+                updateDate: "2022.03.04",
+                note: "运行本软件需要Android 7.0或以上版本",
+                downloadLabel: "下 载（Android版）",
+                downloadHref: "http://www.dream7c.com/寒露.apk",
+                thanksText: "感谢您使用 柒幻 寒露",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.1.6&nbsp;(2022.03.04)</p>*界面优化<br/>*支持对放映界面进行相关设置<br/>*支持长按放映键选择放映类型<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.3&nbsp;(2022.02.15)</p>*界面优化<br/>*修复可能出现的显示错误<hr/>' +
+                    '历史版本 beta (2022.02.04)'
+            }
+        ]
+    },
+    DU: {
+        title: "下载 柒幻 扬琴",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 扬琴",
+                version: "v 1.5",
+                supportLabel: "支持浏览器",
+                supportValue: "Chrome",
+                language: "中文(简体)",
+                size: "252 KB",
+                updateDate: "2026.05.19",
+                downloadLabel: "下 载",
+                downloadHref: "http://www.dream7c.com/柒幻 扬琴 v 1.5.rar",
+                thanksText: "感谢您使用 柒幻 扬琴",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.5&nbsp;(2026.05.19)</p>*界面优化<br/>*支持为数据添加分组及对分组管理<br/>*支持调整窗口宽度<br/>*支持调整窗口停靠位置<br/>*优化数据拖拽操作<br/>*修复可能出现的数据丢失问题<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.2&nbsp;(2026.01.20)</p>*界面优化<br/>*支持导入和导出数据<hr/>' +
+                    '<p class="tx3b">beta&nbsp;(2026.01.03)</p>*开放测试'
+            }
+        ]
+    },
+    DV: {
+        title: "下载 柒幻 麦芒",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 麦芒",
+                version: "v 1.2.0.0",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "3.5 MB",
+                updateDate: "2025.07.19",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载",
+                downloadHref: "http://www.dream7c.com/柒幻 麦芒 v 1.2.0.0.rar",
+                thanksText: "感谢您使用 柒幻 麦芒",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.2.0.0&nbsp;(2025.07.19)</p>*正式发布'
+            }
+        ]
+    },
+    FR: {
+        title: "下载 柒幻 霜降",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 霜降",
+                version: "v 3.1.0.5",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "1.25 MB",
+                updateDate: "2022.12.14",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载（Windows版）",
+                downloadHref: "http://www.dream7c.com/柒幻 霜降 v 3.1.0.5.rar",
+                thanksText: "感谢您使用 柒幻 霜降",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;3.1.0.5&nbsp;(2022.12.14)</p>*支持修改输出矩阵的样式<br/>*同步生成分隔符到公式输出的数据<br/>*修复快速生成数据时可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.9.3&nbsp;(2022.02.24)</p>*界面优化<br/>*随机抽样算法优化<br/>*修复重命名时可能出现的显示问题<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.9.0&nbsp;(2022.01.19)</p>*界面优化<br/>*支持逆序排序<br/>*Solstice添加split语句，支持对数据分组<br/>*修复Solstice中可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.7.6&nbsp;(2021.12.19)</p>*界面优化<br/>*支持对选中内容快速生成数据（Alt+Enter）<br/>*支持对文件属性窗口的选中内容操作<br/>*支持在工具栏中取消多窗口模式<br/>*Solstice添加count语句，支持带条件计数<br/>*文件管理器中添加右键菜单<br/>*修复多窗口模式下切换窗口时样本容量显示的错误<br/>*修复多窗口模式下切换窗口时可能出现的定位异常<br/>*修复Solstice部分语句省略后缀时的显示异常<br/>*修复Solstice的sort语句可能出现的无法显示的问题<br/>*修复拖入文件时可能出现的显示问题<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.5.4&nbsp;(2021.10.12)</p>*界面优化<br/>*Solstice添加更多语法与词缀<br/>*支持在数据窗口快速去重<br/>*修复矩阵转置可能出现的错误<br/>*修复可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.4.6&nbsp;(2021.08.25)</p>*添加更多设置选项<br/>*修复可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.4.1&nbsp;(2021.08.13)</p>*界面优化<br/>*支持保留有效位数<br/>*Solstice代码窗口添加工具栏<br/>*修复Solstice代码窗口快捷键无效的问题<br/>*修复可能出现的显示错误<hr/>' +
+                    '历史版本 v 3.0.0.6 (2021.08.10)'
+            },
+            {
+                tabLabel: "Android版",
+                downloadTitle: "柒幻 霜降 Android",
+                version: "v 1.0.1",
+                supportLabel: "支持系统",
+                supportValue: "Android",
+                language: "中文(简体)",
+                size: "2.17 MB",
+                updateDate: "2022.03.05",
+                note: "运行本软件需要Android 7.0或以上版本",
+                downloadLabel: "下 载（Android版）",
+                downloadHref: "http://www.dream7c.com/霜降.apk",
+                thanksText: "感谢您使用 柒幻 霜降",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.1&nbsp;(2022.03.05)</p>*界面优化<br/>*修复可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">beta&nbsp;(2022.02.26)</p>*性能优化<br/>*修复可能出现的显示错误<hr/>' +
+                    '历史版本 alpha (2022.02.22)'
+            }
+        ]
+    },
+    GG: {
+        title: "下载 柒幻 几何精灵 2020",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 几何精灵 2020",
+                version: "v 2.5.1.3",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "3 MB",
+                updateDate: "2020.12.05",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载",
+                downloadHref: "http://www.dream7c.com/柒幻 几何精灵 2020 v 2.5.1.3e.rar",
+                thanksText: "感谢您使用 柒幻 几何精灵",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;2.5.1.3&nbsp;(2020.12.05)</p>*CGFC语法更新，sum,prod,max,min,gcd,lcm支持多参数计算<br/>*优化CGFC代码自动补全<br/>*修复画板-画笔图标显示异常<hr/>' +
+                    '<p class="tx3b">v&nbsp;2.4.9.2&nbsp;(2020.08.28)</p>*几何计算器“矩形-边、对角线”添加输入规范提示和对正方形的判定<br/>*修复可能出现的显示错误<br/>*修复切换语言后字体不一致的问题<hr/>' +
+                    '<p class="tx3b">v&nbsp;2.4.8.0&nbsp;(2020.07.12)</p>*几何计算器“三角形计算”中添加“角-边-角”方法并同步语法到 CGFC<br/>*优化切换几何计算器和函数计算器时可能出现的错误提示<hr/>' +
+                    '<p class="tx3b">v&nbsp;2.4.5.9&nbsp;(2020.03.13)</p>*CGFC添加输入提示<br/>*几何计算器和函数计算器中支持输入根式<br/>*优化几何计算器中的输入规范提示<br/>*修复可能出现的显示错误<br/>*修复调整字体时可能出现的错误<hr/>' +
+                    '历史版本 v 2.2.4.1 (2019.12.01)'
+            }
+        ]
+    },
+    PC: {
+        title: "下载 柒幻 千纸鹤",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 千纸鹤",
+                version: "v 3.1.2.0",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "968 KB",
+                updateDate: "2022.12.14",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载",
+                downloadHref: "http://www.dream7c.com/柒幻 千纸鹤 v 3.1.2.0.rar",
+                thanksText: "感谢您使用 柒幻 千纸鹤",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;3.1.2.0&nbsp;(2022.12.14)</p>*支持实时预览代码效果<br/>*修改生成的HTML代码样式<br/>*修复修改配置时可能出现的错误<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.0.9&nbsp;(2022.04.02)</p>*界面优化<br/>*支持更多自定义设置<br/>*修复可能出现的显示错误<br/>*修复多选单元格后复制可能出现的异常<hr/>' +
+                    '<p class="tx3b">v&nbsp;3.0.0.0&nbsp;beta&nbsp;(2022.03.20)</p>*重新设计界面<br/>*性能优化<br/>*支持更多自定义设置<br/>*移除部分不常用功能<br/>*修复可能出现的显示错误'
+            }
+        ]
+    },
+    RA: {
+        title: "下载 柒幻 谷雨",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 谷雨",
+                version: "v 1.0.0.2",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "212 KB",
+                updateDate: "2023.01.31",
+                note: "运行本软件需要.NET Framework 4.5.2或以上版本",
+                downloadLabel: "下 载",
+                downloadHref: "http://www.dream7c.com/柒幻 谷雨 v 1.0.0.2b.rar",
+                thanksText: "感谢您使用 柒幻 谷雨",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.0.2&nbsp;(2023.01.31)</p>*修复可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">beta&nbsp;(2022.12.16)</p>*界面优化<br/>*支持保存结算结果并统计历史数据<hr/>' +
+                    '<p class="tx3b">alpha&nbsp;(2022.10.08)</p>*修复可能出现的显示错误'
+            }
+        ]
+    },
+    WD: {
+        title: "下载 柒幻 白露",
+        platforms: [
+            {
+                tabLabel: "Windows版",
+                downloadTitle: "柒幻 白露",
+                version: "v 1.0.25",
+                supportLabel: "支持系统",
+                supportValue: "WinAll",
+                language: "中文(简体)",
+                size: "259 MB",
+                updateDate: "2026.02.06",
+                downloadNotice: "服务器维护中，请使用下方临时链接下载",
+                fallbackLinkLabel: "*点此使用临时链接下载",
+                fallbackLinkHref: "https://pan.quark.cn/s/cd108c664c7b",
+                thanksText: "感谢您使用 柒幻 白露",
+                changelogHtml: '<p class="tx3v">更新内容</p><hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.25&nbsp;(2026.02.06)</p>*界面优化<br/>*投递日期默认升序排序<br/>*添加状态：招满/取消<br/>*修复可能出现的显示问题<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.20&nbsp;(2026.01.30)</p>*界面优化<br/>*首页显示进度和记录总数<br/>*优化对流程时间的校验规则<br/>*待投递状态计入总投递数<br/>*待投递状态不必输入投递日期<br/>*待投递状态不能添加流程<br/>*查看流程窗口包含投递日期<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.11&nbsp;(2026.01.24)</p>*界面优化<br/>*根据状态改变记录行背景色<br/>*支持搜索记录<br/>*支持统计数据<br/>*支持全屏显示<br/>*添加状态：待投递<br/>*修复编辑记录时取消后仍然保存流程数据<hr/>' +
+                    '<p class="tx3b">v&nbsp;1.0.4&nbsp;(2026.01.20)</p>*界面优化<br/>*支持过滤结果列<br/>*调整校验规则<br/>*修复可能出现的显示错误<hr/>' +
+                    '<p class="tx3b">beta&nbsp;(2026.01.17)</p>*开放测试'
+            }
+        ]
+    }
+};
+
+function escapeProductDownloadHtml(text) {
+    return String(text)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+}
+
+function getCurrentProductDownloadKey() {
+    var path = window.location.pathname.replace(/\\/g, "/");
+    var match = path.match(/\/pages\/dream7c-([A-Za-z0-9]+)\//i);
+    return match ? match[1].toUpperCase() : "";
+}
+
+function buildDownloadPanelHtml(platform) {
+    var html = "";
+
+    html += '<div class="tx3" style="clear:both;">';
+    html += '<div class="div_download_info">';
+    html += escapeProductDownloadHtml(platform.downloadTitle) + "<br/><br/>";
+    html += "版本：" + escapeProductDownloadHtml(platform.version) + "<br/><br/>";
+    html += escapeProductDownloadHtml(platform.supportLabel) + "：" + escapeProductDownloadHtml(platform.supportValue) + "<br/>";
+    html += "语言：" + escapeProductDownloadHtml(platform.language) + "<br/>";
+    html += "占用内存：" + escapeProductDownloadHtml(platform.size) + "<br/><br/>";
+    html += "更新日期：" + escapeProductDownloadHtml(platform.updateDate);
+
+    if (platform.note) {
+        html += "<br/>提示：" + escapeProductDownloadHtml(platform.note);
+    }
+
+    if (platform.downloadHref && platform.downloadLabel) {
+        html += '<a href="' + escapeProductDownloadHtml(platform.downloadHref) + '" target="_self ">';
+        html += '<div class="btn_download">' + escapeProductDownloadHtml(platform.downloadLabel) + "</div></a>";
+    }
+
+    if (platform.downloadNotice) {
+        html += '<div class="btn_download" style="color: grey;">' + escapeProductDownloadHtml(platform.downloadNotice) + "<br/></div>";
+    }
+
+    if (platform.fallbackLinkHref && platform.fallbackLinkLabel) {
+        html += '<a href="' + escapeProductDownloadHtml(platform.fallbackLinkHref) + '" target="_blank">';
+        html += '<p style="background-color: #88abda; text-align: center; padding: 10px; color: whitesmoke">';
+        html += escapeProductDownloadHtml(platform.fallbackLinkLabel);
+        html += "</p></a>";
+    }
+
+    html += '<p class="tx2" style="text-align:center;">' + escapeProductDownloadHtml(platform.thanksText) + "</p>";
+    html += "</div>";
+    html += '<div class="div_download_log">' + platform.changelogHtml + "</div>";
+    html += "</div>";
+
+    return html;
+}
+
+function renderProductDownloadPage() {
+    var key = getCurrentProductDownloadKey();
+    var data = productDownloadPageData[key];
+    var titleNode = document.getElementById("product-download-title");
+    var tabsNode = document.getElementById("product-download-tabs");
+    var panelsNode = document.getElementById("product-download-panels");
+    var tabsHtml = "";
+    var panelsHtml = "";
+    var i;
+
+    if (!data) {
+        return;
+    }
+
+    if (titleNode) {
+        titleNode.textContent = data.title;
+        document.title = data.title;
+    }
+
+    if (tabsNode) {
+        if (data.platforms.length > 1) {
+            for (i = 0; i < data.platforms.length; i++) {
+                tabsHtml += '<div class="btn_pos" id="btn_pos' + (i + 1) + '"' +
+                    (i === 0 ? ' style="margin-left:20%;"' : ' style="background-color:#FFFFFF;color:#88abda"') +
+                    '>' + escapeProductDownloadHtml(data.platforms[i].tabLabel) + "</div>";
+            }
+        }
+        tabsNode.innerHTML = tabsHtml;
+    }
+
+    if (panelsNode) {
+        for (i = 0; i < data.platforms.length; i++) {
+            panelsHtml += '<div class="mdiv" id="pos' + (i + 1) + '"' + (i > 0 ? ' style="display:none;"' : '') + ">";
+            panelsHtml += buildDownloadPanelHtml(data.platforms[i]);
+            panelsHtml += "</div>";
+        }
+        panelsNode.innerHTML = panelsHtml;
+    }
+}
+
+function setupProductDownloadTabs() {
+    var key = getCurrentProductDownloadKey();
+    var data = productDownloadPageData[key];
+    var i;
+
+    if (!data || data.platforms.length <= 1) {
+        return;
+    }
+
+    function display(index) {
+        var k;
+        var panel;
+        var button;
+
+        for (k = 1; k <= data.platforms.length; k++) {
+            panel = document.getElementById("pos" + k);
+            button = document.getElementById("btn_pos" + k);
+
+            if (panel) {
+                panel.style.display = "none";
+            }
+            if (button) {
+                button.style.backgroundColor = "#FFFFFF";
+                button.style.color = "#88abda";
+            }
+        }
+
+        panel = document.getElementById("pos" + index);
+        button = document.getElementById("btn_pos" + index);
+
+        if (panel) {
+            panel.style.display = "block";
+        }
+        if (button) {
+            button.style.backgroundColor = "#88abda";
+            button.style.color = "#FFFFFF";
+        }
+    }
+
+    for (i = 1; i <= data.platforms.length; i++) {
+        document.getElementById("btn_pos" + i).onclick = (function(index) {
+            return function() {
+                display(index);
+            };
+        })(i);
+    }
+
+    display(1);
+}
